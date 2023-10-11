7c8ec2c76ea9bb14c7d86e5b19cfe2ff06f1b4a1
vasuatgiet <vasuatgiet@gmail.com>
2023-10-11 13:25:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,31 +20,39 @@ provider "abbey" {
   bearer_auth = var.abbey_token
 }
 
-/* Uncomment and update
-resource "abbey_grant_kit" "abbey_demo_site" {
-  name = "Abbey_Demo_Site"
-  description = <<-EOT
-    Grants access to Abbey's Demo Page.
-  EOT
+resource "abbey_grant_kit" "Abbey_demo" {
+  name = "Abbey_demo"
+  description = "Abbey conf demo"
 
   workflow = {
     steps = [
       {
         reviewers = {
-          one_of = ["replace-me@example.com"] # CHANGEME
+          one_of = [
+            "vasuatgiet@gmail.com"
+          ]
         }
       }
     ]
   }
 
   policies = [
-    { bundle = "github://replace-me-with-organization/replace-me-with-repo/policies" } # CHANGEME
+    {
+      query = <<-EOT
+        package common
+        
+        import data.abbey.functions
+        
+        allow[msg] {
+          functions.expire_after("5m")
+          msg := sprintf("granting access for %s", ["5m"])
+        }
+      EOT
+    }
   ]
 
   output = {
-    # Replace with your own path pointing to where you want your access changes to manifest.
-    # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
-    location = "github://replace-me-with-organization/replace-me-with-repo/access.tf" # CHANGEME
+    location = "github://vasuatgiet/abbey-starter-kit-quickstart/access.tf"
     append = <<-EOT
       resource "abbey_demo" "grant_read_write_access" {
         permission = "read_write"
@@ -53,4 +61,3 @@ resource "abbey_grant_kit" "abbey_demo_site" {
     EOT
   }
 }
-*/
